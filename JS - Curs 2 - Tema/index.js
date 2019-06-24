7486a7dd4d0b723f5f803dc8add499b20220ca07
Andrei Stoleriu <andrei.stoleriu23@gmail.com>
2019-06-24 22:34:06 +0300

    tema1 javascript

---
+++ JS - Curs 2 - Tema/index.js
@@ -0,0 +1,50 @@
+// //n! FACTORIAL - exercitiul 1
+
+// var factorial = 1;
+
+// for (var i=1; i < 7; i++) {
+//     console.log(factorial = factorial * (i+1));
+// }
+
+// //cmmdc - exercitiul 2
+// function cmmdc (x, y) {
+//     if (x % y === 0) {
+//         return y;
+//     } else {
+//         return cmmdc (y, x % y)
+//     }
+// }
+// console.log(cmmdc(1654, 169));
+
+// //CELSIUS TO FAHRENHEIT - exercitiul 3
+
+// var celsius = prompt('ce temperatura este?');
+// var fahrenheit = celsius * 9/5 + 32;
+// alert(fahrenheit);
+
+// //FAHRENHEIT TO CELSIUS
+
+// var fahrenheit = prompt('ce temperatura este?');
+// var celsius = (fahrenheit-32)/(9/5);
+// alert(celsius);
+
+
+// // CELSIUS TO FAHRENHEIT WITH FUNCTION
+
+// function celsiusFahr(temperature) {
+//     var celsius = temperature;
+//     var fahrenheit = celsius * 9/5 + 32;
+//     console.log('temperatura este '+ fahrenheit);
+// }
+// celsiusFahr(50);
+
+//FAHRENHEIT TO CELSIUS
+
+// function fahrCelsius(temperature) {
+//     var fahrenheit = temperature;
+//     var celsius = (fahrenheit-32)/(9/5);
+//     console.log('temperatura este '+ celsius);
+// }
+// fahrCelsius(100);
+
+
---
+++ JS - Curs 2/index.js
@@ -0,0 +1,70 @@
+// var age = Number(prompt("Care este varsta ta?"));
+
+// if (age < 15) {
+//     alert('primesti reducere 25%');
+// }
+//  else if (age >= 15 && age < 25) {
+//     alert('primesti reducere 10%');
+// } else {
+//     alert('nu primesti nicio reducere');
+// }
+
+// switch (age) {
+//   case 15:
+//     alert("25% reducere");
+//     break;
+//   case 16:
+//     alert("20% reducere");
+//     break;
+//   case 17:
+//     alert("15% reducere");
+//     break;
+//   case 18:
+//     alert("10% reducere");
+//     break;
+//   default:
+//     alert("fara reducere");
+// }
+
+//NUMERE PARE
+
+// for(var i=0; i<100; i=i+2) { => numere pare
+//     console.log(i)
+// }
+
+//NUMERE IMPARE
+
+// for (var i=0; i<100; i++) {
+//     console.log(i);
+// }
+
+// DESCRESCATOR
+
+// for (var i=100; i>0; i--) {
+//     console.log(i);
+// }
+
+// SUMA NUMERELOR
+
+// var sum = 0;
+// for (var i=0; i<100; i++){
+//     console.log(sum=sum+(i+1));
+// }
+
+//
+
+//WHILE LOOP
+
+// var i = 0;
+// while (i< 100) {
+//     console.log(i);
+//     i++
+
+// var i = 0;
+// while (i< 100) {
+//     console.log(i);
+
+
+
+
+
